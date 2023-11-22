06676c8682651eb7ca7d7cf6ee6f7a8846c9e905
seonmiki <seonmiki@student.42seoul.kr>
2023-11-22 21:21:54 +0900

    일단 돌아는 감

--- source/execute_pipe.c
+++ source/execute_pipe.c
@@ -18,8 +18,10 @@ void    exeute_pipe(t_list *proc_lst)
     pid_t   pid;
     int     fd[2];
     int     child_cnt;
+	t_list	*before;
 
     child_cnt = 0;
+	before = NULL;
     while (proc_lst)
     {
 		if (((t_proc_info *)(proc_lst->content))->cmd_path == NULL)
@@ -34,11 +36,14 @@ void    exeute_pipe(t_list *proc_lst)
             perror("fork error");
 		if (pid == 0)
 		{
-			dup2(((t_proc_info *)(proc_lst->content))->prev, ((t_proc_info *)(proc_lst->content))->in_fd);
-			if (proc_lst->next == NULL)
-				dup2(((t_proc_info *)(proc_lst->content))->out_fd, STDOUT_FILENO);
+			if (before && ((t_proc_info *)(proc_lst->content))->in_fd == STDIN_FILENO) //이전 노드가 있고, 현재 노드의 infile이 stdin일 경우
+				dup2(((t_proc_info *)(before->content))->prev, STDIN_FILENO); //이전 노드 파이프의 읽기 종단을 stdin으로
+			else                                                                 //이전 노드가 없거나, 현재 노드의 infile이 stdin이 아닐 경우
+				dup2(((t_proc_info *)(proc_lst->content))->in_fd, STDIN_FILENO); //현재 노드의 infile을 stdin으로
+			if (proc_lst->next != NULL && ((t_proc_info *)(proc_lst->content))->out_fd == STDOUT_FILENO) //현재 노드의 outfile이 stdout일 경우(단, 마지막이면 패스)
+				dup2(fd[WRITE], STDOUT_FILENO); //파이프의 쓰기 종단을 stdout으로
 			else
-				dup2(fd[WRITE], ((t_proc_info *)(proc_lst->content))->out_fd);
+				dup2(((t_proc_info *)(proc_lst->content))->out_fd, STDOUT_FILENO); //현재 노드의 outfile을 stdout으로
 			execve(((t_proc_info *)(proc_lst->content))->cmd_path, 
 					((t_proc_info *)(proc_lst->content))->cmd_argv, 
 					((t_proc_info *)(proc_lst->content))->envp);
@@ -46,10 +51,15 @@ void    exeute_pipe(t_list *proc_lst)
 		}
 		else
 		{
-			dup2(fd[READ], ((t_proc_info *)(proc_lst->content))->prev);
+			// if (proc_lst->next == NULL)
+				// close(fd[READ]);
+			// else
+				((t_proc_info *)(proc_lst->content))->prev = fd[READ];
+				// dup2(fd[READ], ((t_proc_info *)(proc_lst->content))->prev);
 			close(fd[WRITE]);
 		}
 		child_cnt++;
+		before = proc_lst;
         proc_lst = proc_lst->next;
     }
 	if (child_cnt)
