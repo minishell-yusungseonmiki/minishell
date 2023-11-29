248b57f7661119befffda3f0e0b5f2b889d3a3f4
jin20fd <yusung@cx1r4s3.42seoul.kr>
2023-11-29 17:04:37 +0900

    exit norm 완성

--- source/builtin_exit.c
+++ source/builtin_exit.c
@@ -1,18 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtin_exit.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yusung <yusung@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/29 16:55:58 by yusung            #+#    #+#             */
+/*   Updated: 2023/11/29 17:01:44 by yusung           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../include/minishell.h"
 
-void	execute_exit(t_proc_info *proc, int only_builtin)
+void	cal_exit_status(char *first_arg)
 {
-	char	*first_arg;
-	int		i;
-
-	if (only_builtin) //builtin 하나인 경우만 exit 출력
-		printf("exit\n");
-	first_arg = proc->cmd_argv[1];
-	if (first_arg == NULL)
+	g_exit_status = ft_atoi(first_arg);
+	while (g_exit_status < 0)
 	{
-		g_exit_status = 0;
-		exit(g_exit_status);
+		g_exit_status *= -1;
+		g_exit_status = 256 - g_exit_status;
 	}
+	g_exit_status = g_exit_status % 256;
+}
+
+void	check_numeric_first_arg(char *first_arg)
+{
+	int	i;
+
 	i = 0;
 	while (first_arg[i])
 	{
@@ -26,14 +40,22 @@ void	execute_exit(t_proc_info *proc, int only_builtin)
 		}
 		i++;
 	}
-	g_exit_status = ft_atoi(first_arg);
-	while (g_exit_status < 0)
+}
+
+void	execute_exit(t_proc_info *proc, int only_builtin)
+{
+	char	*first_arg;
+
+	if (only_builtin) //builtin 하나인 경우만 exit 출력
+		printf("exit\n");
+	first_arg = proc->cmd_argv[1];
+	if (first_arg == NULL)
 	{
-		g_exit_status *= -1;
-		g_exit_status = 256 - g_exit_status;
+		g_exit_status = 0;
+		exit(g_exit_status);
 	}
-	g_exit_status = g_exit_status % 256;
-	//printf("ec : %d\n", g_exit_status);
+	check_numeric_first_arg(first_arg);
+	cal_exit_status(first_arg);
 	if (proc->cmd_argv[2]) // 인자가 두개 이상이면
 		printf("exit: too many arguments\n");
 	exit(g_exit_status);
